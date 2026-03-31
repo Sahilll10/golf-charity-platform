9dd8a17cc08e51878365adc6f51ae0cf4c6e942c
Learning <sahilgreat321@gmail.com>
2026-03-31 18:41:49 +0530

    Fix hoempage

--- golf-charity-platform/src/app/page.tsx
+++ golf-charity-platform/src/app/page.tsx
@@ -1,5 +1,8 @@
-import { redirect } from 'next/navigation'
-
-export default function RootPage() {
-  redirect('/')
+export default function Home() {
+  return (
+    <div style={{padding: "40px"}}>
+      <h1>Golf Charity Platform 🚀</h1>
+      <p>Your app is now working!</p>
+    </div>
+  );
 }
